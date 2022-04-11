00095858d0bc67f353e267d75098cad98a3661e4
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2022-04-11 20:52:23 +0000

    improve placeholder

--- packages/browser-extension/src/components/popup-window.tsx
+++ packages/browser-extension/src/components/popup-window.tsx
@@ -141,7 +141,7 @@ export const PopupWindow = () => {
             autoFocus
             value={query}
             onChange={(e) => setQuery(e.target.value)}
-            placeholder='Search ("/")'
+            placeholder="Search by title or metadata [press / to focus]"
           />
         </div>
       </div>
